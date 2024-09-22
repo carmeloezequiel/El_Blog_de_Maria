d5805a6e08b752e24dbbc50fbc4259ba267114ea
Carmelo <carmeloezequiel98@gmail.com>
2024-09-21 23:04:54 -0400

    foto avatar lista, footer decente

--- El_Blog_de_Maria/components/footer.py
+++ El_Blog_de_Maria/components/footer.py
@@ -5,11 +5,14 @@ import  El_Blog_de_Maria.styles.styles as style
 
 def footer() -> rx.Component:
     return rx.vstack(
-        rx.image(src='favicon.ico'),
-        rx.text(f'2020-{datetime.date.today().year} El Blog de Maria by Maria',
-                font_size=style.Spacer.MEDIUM.value),
+        rx.image(src='banner_maria_c.jpg', max_width=style.MAX_WIDTH, border_radius=style.Spacer.SMALL),
+        rx.text(f'2020-{datetime.date.today().year} El Block de Maria by Maria',
+                font_size=style.Spacer.MEDIUM.value,
+                position='absolute',  # Establece la posición absoluta para superponer el texto
+                bottom=0,  # Ajusta la posición vertical del texto (puedes experimentar con diferentes valores)
+                padding=style.Spacer.SMALL.value),  # Agrega un poco de espacio alrededor del texto
         margin_y=style.Spacer.BIG.value,
         margin_x=style.Spacer.SMALL.value,
-        width='100%',
+        width='99%',
         align_items='center'
     )
--- El_Blog_de_Maria/views/header/header.py
+++ El_Blog_de_Maria/views/header/header.py
@@ -1,19 +1,20 @@
 import reflex as rx 
 import El_Blog_de_Maria.styles.styles as styles
 from El_Blog_de_Maria.components.link_icon import link_icon
+import El_Blog_de_Maria.styles.colors as Color
 
 def header() -> rx.Component:
     return rx.vstack(
         rx.hstack(
-            rx.avatar(fallback="MR",radius="full", size='9'),
+            rx.avatar(src='avatar4.jpg',fallback="MR",radius="full", size='9',padding=styles.Spacer.SMALL.value, background=Color.Color.CONTENT),
             rx.vstack(
-                rx.heading('El Blog de María', 
+                rx.heading('El Block de María', 
                            size='7',
                            margin_top=f'{styles.Spacer.MEDIUM_BIG.value} !important',
                            margin_bottom='0px !important'
                            ),
                         
-                rx.text('@elblogdemaria', margin_top='0px !important'),
+                rx.text('Papelería bonita, bullet journal, ArtJournal y scrapbooking ✨', margin_top='0px !important'),
                 rx.hstack(
                     link_icon('https://www.youtube.com/watch?v=n2YrGsXJC6Y&t=12397s'),
                 ),
